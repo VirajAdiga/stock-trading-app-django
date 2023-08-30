feaef1a8527cdaf14c3566cc1f8b42e9ec56de58
VirajAdiga <virajadigamaclub@gmail.com>
2023-08-30 11:42:53 +0530

    Add home page for stock picking

---
+++ watchlist/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("", views.stock_picker, name="stock-picker")
+]
--- watchlist/views.py
+++ watchlist/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def stock_picker(request):
+    return render(request, "watchlist/stock_picker.html")
